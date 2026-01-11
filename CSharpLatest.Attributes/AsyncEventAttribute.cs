bd838cee40fb6ab2bc420608b32b6edc7c54fbe4
David Le Bansais <dlebansais@gmail.com>
2026-01-11 22:20:26 +0100

    Fixed the AsyncEvent attribute target.

--- CSharpLatest.Attributes/AsyncEventAttribute.cs
+++ CSharpLatest.Attributes/AsyncEventAttribute.cs
@@ -5,7 +5,7 @@ using System;
 /// <summary>
 /// Represents an attribute to automatically generate an event that can run handlers using async/await.
 /// </summary>
-[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Event, Inherited = false, AllowMultiple = false)]
 public sealed class AsyncEventAttribute : Attribute
 {
 }
